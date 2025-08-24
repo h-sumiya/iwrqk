2a5b23b7d9c7b3f7423845df54b85e7ad69785f0
Huang Wen <46584904+bilibili10633@users.noreply.github.com>
2025-08-24 13:41:16 +0800

    fix search

--- lib/app/components/user_preview/users_preview_list/repository.dart
+++ lib/app/components/user_preview/users_preview_list/repository.dart
@@ -24,7 +24,7 @@ class UsersPreviewListRepository {
         break;
       case UsersSourceType.search:
         path = "/search";
-        queryParameters.addAll({"type": "user", "query": sortSetting.keyword});
+        queryParameters.addAll({"type": "users", "query": sortSetting.keyword});
         break;
     }
 
